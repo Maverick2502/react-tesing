5065ebe456592c0ed440220dab74b3112cdd8758
Nurullo <khuwaidullaev@gmail.com>
2021-08-02 07:22:21 +0500

    Testing Async code

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Async from "./Async";
 
 function Greeting() {
   const [changedText, setChangedText] = useState(false);
---
+++ src/components/Output.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Output(props) {
+  return <h3>{props.children}</h3>;
+}
+
+export default Output;
